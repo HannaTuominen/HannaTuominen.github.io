2ea3a5ccb896c30916c79949fd9ecc3b653f3599
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-02-02 20:11:00 +0200

    Update some links to have pointer cursor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
+// must use hasrouter for github.io to be able to show at least somewhat pages straight from the search bar
 const app = (
   <BrowserRouter>
     <App/>
