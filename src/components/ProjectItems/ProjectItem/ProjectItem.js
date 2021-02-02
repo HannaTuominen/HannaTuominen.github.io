23792beb09c326b41f29ee32e5fda508036ad1ba
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-02-02 13:27:53 +0200

    Update website outlook on mobile and web

--- src/components/ProjectItems/ProjectItem/ProjectItem.js
+++ src/components/ProjectItems/ProjectItem/ProjectItem.js
@@ -3,12 +3,32 @@ import React from 'react';
 import './ProjectItem.css';
 
 const ProjectItem = ( props ) => {
+  let image;
+
+  switch (props.type) {
+    case (1):
+      image = <div className="ProjectItemContainerImageWeb">
+        <img src={props.image} alt={props.title}/>
+      </div>
+      break;
+    case (2):
+      image = <div className="ProjectItemContainerImageMobile">
+        <img src={props.image} alt={props.title}/>
+      </div>
+      break;
+    case (3):
+      image = <div className="ProjectItemContainerImageOther">
+        <img src={props.image} alt={props.title}/>
+      </div>
+      break;
+    default:
+      image = null;
+  }
+
 
   return (
     <div className="ProjectItemContainer">
-      <div className="ProjectItemContainerImage">
-        <img src={props.image} alt={props.title}/>
-      </div>
+      {image}
       <div className="ProjectItemContainerInfo">
         <h3>{props.title}</h3>
         <h4>{props.desc}</h4>
