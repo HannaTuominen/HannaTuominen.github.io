7488c309ef3c740c142b3050afb1b9fefe026fb0
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-02-02 19:44:06 +0200

    Move web link before mobile

--- src/components/Navigation/Navigation/Navigation.js
+++ src/components/Navigation/Navigation/Navigation.js
@@ -6,8 +6,8 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 const navigation = (props) => (
   <ul className="NavigationItems">
     <NavigationItem link="/about">About</NavigationItem>
-    <NavigationItem link="/projects/mobile">Mobile</NavigationItem>
     <NavigationItem link="/projects/web">Web</NavigationItem>
+    <NavigationItem link="/projects/mobile">Mobile</NavigationItem>
     <NavigationItem link="/projects/other">Other</NavigationItem>
   </ul>
 );
