f9873207f7e3537b0804f6bbe452cc5ba63aab4d
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-02-02 14:56:50 +0200

    Update logo, favicon and title

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,6 +3,8 @@ import './Toolbar.css';
 import {useHistory} from 'react-router-dom';
 import Navigation from "../Navigation/Navigation";
 import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import Logo from '../../../assets/logo.png';
+
 
 const Toolbar = (props) => {
 
@@ -14,7 +16,7 @@ const Toolbar = (props) => {
 
   return (
   <header className="Toolbar">
-   <div onClick={getToHome} className="HomeLogo">Hanna's portfolio</div>
+    <img src={Logo} onClick={getToHome} className="HomeLogo" style={{height:"50px"}}/>
     <DrawerToggle clicked={props.drawerToggleClicked}/>
     <nav className="DesktopOnly">
       <Navigation/>
